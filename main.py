3347fb2fccbc638d23b9adcff8cbe6a1d987dfbc
traceback <mehran1990ss@yahoo.com>
2025-05-11 14:52:23 +0330

    333

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from telegram import Bot
 
 # دریافت توکن و چت آیدی از محیط
 TOKEN = os.getenv("7758220854:AAFVjFJJNTGwIKGl72hZpMCcVSa1xqO68_s")
-CHAT_ID = "90476610"  # چت آیدی شما
+CHAT_ID = os.getenv("90476610")
 
 def send_test():
     try:
