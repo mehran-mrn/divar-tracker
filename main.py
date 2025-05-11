90780e9f53736346c711385131c9999487226cfe
traceback <mehran1990ss@yahoo.com>
2025-05-11 15:54:57 +0330

    333

--- main.py
+++ main.py
@@ -23,14 +23,15 @@ PRICE_SELECTOR = "div.unsafe-kt-post-card__description"  # انتخاب قیمت
 
 def init_driver():
     chrome_options = Options()
-    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36")
     chrome_options.add_argument("--disable-blink-features=AutomationControlled")
     chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
     chrome_options.add_experimental_option("useAutomationExtension", False)
-    
+    chrome_options.binary_location = "/usr/bin/chromium-browser"
     return webdriver.Chrome(options=chrome_options)
 
 def load_history():
