55e7a05f9ab1c477507c26200a71e2c9da2871a6
traceback <mehran1990ss@yahoo.com>
2025-05-11 13:53:44 +0330

    Add test version

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+from telegram import Bot
+
+# دریافت توکن و چت آیدی از محیط
+TOKEN = os.getenv("7758220854:AAFVjFJJNTGwIKGl72hZpMCcVSa1xqO68_s")
+CHAT_ID = "-1002527901719"  # چت آیدی شما
+
+def send_test():
+    try:
+        bot = Bot(token=TOKEN)
+        bot.send_message(
+            chat_id=CHAT_ID,
+            text="✅ تست موفقیت‌آمیز! GitHub Actions به تلگرام متصل شد!"
+        )
+        print("پیام با موفقیت ارسال شد!")
+    except Exception as e:
+        print(f"خطا در ارسال پیام: {e}")
+
+if __name__ == "__main__":
+    send_test()
